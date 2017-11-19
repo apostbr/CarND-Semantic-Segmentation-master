660dd6b8854307467c08beced6c7e18bef21d39f
Alexandre Nogueira <alexandre.nogueira@gmail.com>
2017-11-19 13:37:50 -0200

    Add files via upload

---
+++ gpu_test.py
@@ -0,0 +1,16 @@
+import os.path
+import tensorflow as tf
+import warnings
+from distutils.version import LooseVersion
+import project_tests as tests
+
+
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
+
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
